b83afe281f3b7d2c12789a13755e4a676ebda293
roman.saldan <80323932597>
2017-02-09 12:12:20 +0200

    Leads list UI -> done

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/holders/view/crm/LeadVH.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/holders/view/crm/LeadVH.java
@@ -4,8 +4,10 @@ import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
 import android.text.TextUtils;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.facebook.stetho.common.StringUtil;
 import com.michenko.simpleadapter.OnCardClickListener;
 import com.thinkmobiles.easyerp.R;
 import com.thinkmobiles.easyerp.presentation.base.rules.MasterFlowSelectableVHHelper;
@@ -13,6 +15,7 @@ import com.thinkmobiles.easyerp.presentation.custom.RoundRectDrawable;
 import com.thinkmobiles.easyerp.presentation.holders.data.crm.LeadDH;
 import com.thinkmobiles.easyerp.presentation.managers.DateManager;
 import com.thinkmobiles.easyerp.presentation.managers.TagHelper;
+import com.thinkmobiles.easyerp.presentation.utils.Constants;
 
 /**
  * Created by Lynx on 1/16/2017.
@@ -22,24 +25,20 @@ public final class LeadVH extends MasterFlowSelectableVHHelper<LeadDH> {
 
     private final TextView tvLeadName_LIL;
     private final TextView tvStage_LIL;
-    private final TextView tvPriority_LIL;
+    private final ImageView ivPriority_LIL;
     private final TextView tvSource_LIL;
     private final TextView tvAssignedTo_LIL;
     private final TextView tvEditedBy_VLIL;
-
-    private final String noData;
 
     public LeadVH(View itemView, @Nullable OnCardClickListener listener, int viewType) {
         super(itemView, listener, viewType);
 
         tvLeadName_LIL = findView(R.id.tvLeadName_LIL);
         tvStage_LIL = findView(R.id.tvStage_LIL);
-        tvPriority_LIL = findView(R.id.tvPriority_LIL);
+        ivPriority_LIL = findView(R.id.ivPriority_LIL);
         tvSource_LIL = findView(R.id.tvSource_LIL);
         tvAssignedTo_LIL = findView(R.id.tvAssignedTo_LIL);
         tvEditedBy_VLIL = findView(R.id.tvEditedBy_VLIL);
-
-        noData = itemView.getContext().getString(R.string.no_data);
     }
 
     @Override
@@ -49,28 +48,39 @@ public final class LeadVH extends MasterFlowSelectableVHHelper<LeadDH> {
         if(!TextUtils.isEmpty(data.getLeadItem().name))
             tvLeadName_LIL.setText(data.getLeadItem().name);
         else
-            tvLeadName_LIL.setText(noData);
-        if(data.getLeadItem().workflow != null && !TextUtils.isEmpty(data.getLeadItem().workflow.name))
+            tvLeadName_LIL.setText("");
+        if(data.getLeadItem().workflow != null && !TextUtils.isEmpty(data.getLeadItem().workflow.name)) {
             tvStage_LIL.setText(data.getLeadItem().workflow.name);
-        else
-            tvStage_LIL.setText(noData);
+            tvStage_LIL.setBackground(new RoundRectDrawable(ContextCompat.getColor(itemView.getContext(), TagHelper.getStatusColorRes(data.getLeadItem().workflow.name))));
+        }
+        else {
+            tvStage_LIL.setBackgroundResource(0);
+        }
         if(!TextUtils.isEmpty(data.getLeadItem().priority)) {
-            tvPriority_LIL.setText(data.getLeadItem().priority.toUpperCase());
-            tvPriority_LIL.setBackgroundDrawable(
-                    new RoundRectDrawable(ContextCompat.getColor(itemView.getContext(), TagHelper.getColorResIdByName(data.getLeadItem().priority))));
-            tvPriority_LIL.setVisibility(View.VISIBLE);
+            switch (data.getLeadItem().priority) {
+                case "Hot":
+                    ivPriority_LIL.setImageDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_high_priority_20));
+                    break;
+                case "Medium":
+                    ivPriority_LIL.setImageDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_medium_priority_20));
+                    break;
+                case "Cold":
+                    ivPriority_LIL.setImageDrawable(ContextCompat.getDrawable(itemView.getContext(), R.drawable.ic_low_priority_20));
+                    break;
+            }
+            ivPriority_LIL.setVisibility(View.VISIBLE);
         } else {
-            tvPriority_LIL.setText(null);
-            tvPriority_LIL.setVisibility(View.GONE);
+            ivPriority_LIL.setImageResource(0);
+            ivPriority_LIL.setVisibility(View.GONE);
         }
         if(!TextUtils.isEmpty(data.getLeadItem().source))
             tvSource_LIL.setText(data.getLeadItem().source);
         else
-            tvSource_LIL.setText(noData);
+            tvSource_LIL.setText("");
         if(data.getLeadItem().salesPerson != null && !TextUtils.isEmpty(data.getLeadItem().salesPerson.name))
             tvAssignedTo_LIL.setText(data.getLeadItem().salesPerson.name);
         else
-            tvAssignedTo_LIL.setText(noData);
+            tvAssignedTo_LIL.setText("");
         if(data.getLeadItem().editedBy != null && !TextUtils.isEmpty(data.getLeadItem().editedBy.date)) {
             //Last Edited: Today, at 2:45 PM by Test Admin
             String strDate = data.getLeadItem().editedBy.date;
@@ -79,7 +89,7 @@ public final class LeadVH extends MasterFlowSelectableVHHelper<LeadDH> {
                 out = out + " by " + data.getLeadItem().editedBy.user;
             tvEditedBy_VLIL.setText(out);
         } else
-            tvEditedBy_VLIL.setText(noData);
+            tvEditedBy_VLIL.setText("");
 
         tvLeadName_LIL.requestLayout();
     }
--- app/src/main/java/com/thinkmobiles/easyerp/presentation/managers/TagHelper.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/managers/TagHelper.java
@@ -9,46 +9,72 @@ import com.thinkmobiles.easyerp.R;
 public abstract class TagHelper {
     /**
      * "tag_bg_red
-     "tag_bg_pink
-     "tag_bg_orange
-     "tag_bg_yellow
-     "tag_bg_green_dark
-     "tag_bg_green_light
-     "tag_bg_blue_light
-     "tag_bg_blue_dark
-     "tag_bg_purple
-     "tag_bg_black
-     "tag_bg_grey
+     * "tag_bg_pink
+     * "tag_bg_orange
+     * "tag_bg_yellow
+     * "tag_bg_green_dark
+     * "tag_bg_green_light
+     * "tag_bg_blue_light
+     * "tag_bg_blue_dark
+     * "tag_bg_purple
+     * "tag_bg_black
+     * "tag_bg_grey
      */
 
-    private static final String COLOR_RED           = "bgRed";
-    private static final String COLOR_PINK          = "bgPink";
-    private static final String COLOR_ORANGE        = "bgOrange";
-    private static final String COLOR_YELLOW        = "bgYellow";
-    private static final String COLOR_GREEN_DARK    = "bgGreenDark";
-    private static final String COLOR_GREEN_LIGHT   = "bgGreenLight";
-    private static final String COLOR_BLUE_LIGHT    = "bgBlueLight";
-    private static final String COLOR_BLUE_DARK     = "bgBlueDark";
-    private static final String COLOR_PURPLE        = "bgPurple";
-    private static final String COLOR_BLACK         = "bgBlack";
-    private static final String COLOR_GREY          = "bgGrey";
+    private static final String COLOR_RED = "bgRed";
+    private static final String COLOR_PINK = "bgPink";
+    private static final String COLOR_ORANGE = "bgOrange";
+    private static final String COLOR_YELLOW = "bgYellow";
+    private static final String COLOR_GREEN_DARK = "bgGreenDark";
+    private static final String COLOR_GREEN_LIGHT = "bgGreenLight";
+    private static final String COLOR_BLUE_LIGHT = "bgBlueLight";
+    private static final String COLOR_BLUE_DARK = "bgBlueDark";
+    private static final String COLOR_PURPLE = "bgPurple";
+    private static final String COLOR_BLACK = "bgBlack";
+    private static final String COLOR_GREY = "bgGrey";
 
-    public static final String COLD                 = "Cold";
-    public static final String MEDIUM               = "Med";
-    public static final String HOT                  = "Hot";
+    public static final String COLD = "Cold";
+    public static final String MEDIUM = "Med";
+    public static final String HOT = "Hot";
 
-    public static final String PENDING              = "Pending";
-    public static final String NEW                  = "New";
-    public static final String IN_PROGRESS          = "In Progress";
-    public static final String CANCELLED            = "Cancelled";
-    public static final String DONE                 = "Done";
-    public static final String UNLINKED             = "UnLinked";
+    public static final String PENDING = "Pending";
+    public static final String NEW = "New";
+    public static final String IN_PROGRESS = "In Progress";
+    public static final String CANCELLED = "Cancelled";
+    public static final String DONE = "Done";
+    public static final String UNLINKED = "UnLinked";
 
-    public static final String PAID                 = "Paid";
-    public static final String UNPAID               = "Unpaid";
-    public static final String UNPAID_NOT_APPROVED  = "Unpaid / Not Approved";
-    public static final String PARTIALLY_PAID       = "Partially Paid";
-    public static final String DRAFT                = "Draft";
+    public static final String PAID = "Paid";
+    public static final String UNPAID = "Unpaid";
+    public static final String UNPAID_NOT_APPROVED = "Unpaid / Not Approved";
+    public static final String PARTIALLY_PAID = "Partially Paid";
+    public static final String DRAFT = "Draft";
+
+    public static final String PRIORITY_NEW = "New";
+    public static final String PRIORITY_SENT = "Sent";
+    public static final String PRIORITY_INTERESTED = "Interested";
+    public static final String PRIORITY_NOT_INTERESTED = "Not Interested";
+    public static final String PRIORITY_NO_ANSWER = "No Answer";
+    public static final String PRIORITY_DONT_CONTACT = "Don't Contact";
+
+    public static int getStatusColorRes(String status) {
+        switch (status) {
+            case PRIORITY_NEW:
+                return R.color.color_status_new;
+            case PRIORITY_SENT:
+                return R.color.color_status_sent;
+            case PRIORITY_INTERESTED:
+                return R.color.color_status_interested;
+            case PRIORITY_NOT_INTERESTED:
+                return R.color.color_status_not_interested;
+            case PRIORITY_NO_ANSWER:
+                return R.color.color_status_no_answer;
+            case PRIORITY_DONT_CONTACT:
+                return R.color.color_status_do_not_contact;
+            default:
+                return R.color.color_status_do_not_contact;
+        }
+    }
 
     public static int getColorResIdByName(String colorName) {
         switch (colorName) {
