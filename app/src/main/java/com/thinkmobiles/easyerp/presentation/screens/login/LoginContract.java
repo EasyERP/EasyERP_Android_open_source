0981664f0e058691c943959ef4b348c87bfe6ee7
roman.saldan <80323932597>
2017-02-14 14:30:28 +0200

    add demo button and fake credentials for it

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/login/LoginContract.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/login/LoginContract.java
@@ -29,6 +29,7 @@ public interface LoginContract {
     }
     interface LoginPresenter extends BasePresenter {
         void login();
+        void launchDemoMode();
         void getCurrentUser();
 
         void clearCookies();
