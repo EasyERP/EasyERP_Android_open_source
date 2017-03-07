fe79a430c101fe199aa5e5f7108ea3dfc8751658
roman.saldan <80323932597>
2017-03-07 16:18:36 +0200

    Add icons

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/holders/view/inventory/GoodOutNoteVH.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/holders/view/inventory/GoodOutNoteVH.java
@@ -1,4 +1,4 @@
-package com.thinkmobiles.easyerp.presentation.holders.view.crm;
+package com.thinkmobiles.easyerp.presentation.holders.view.inventory;
 
 import android.support.annotation.Nullable;
 import android.support.v4.content.ContextCompat;
@@ -62,8 +62,8 @@ public class GoodOutNoteVH extends SelectableVHHelper<GoodOutNoteDH> {
         //TODO uncomment when status appears
         tvStatus_VLIGON.setBackgroundDrawable(new RoundRectDrawable(ContextCompat.getColor(itemView.getContext(), TagHelper.getColorResIdByName(item.workflow.status))));
 
-        ivPrinted_VLIGON.setImageResource(item.printed ? R.drawable.ic_allocated : R.drawable.ic_allocated_off);
-        ivPicked_VLIGON.setImageResource(item.picked ? R.drawable.ic_fulfilled : R.drawable.ic_fulfilled_off);
+        ivPrinted_VLIGON.setImageResource(item.printed ? R.drawable.ic_print : R.drawable.ic_print_off);
+        ivPicked_VLIGON.setImageResource(item.picked ? R.drawable.ic_allocated : R.drawable.ic_allocated_off);
         ivPacked_VLIGON.setImageResource(item.packed ? R.drawable.ic_fulfilled : R.drawable.ic_fulfilled_off);
         ivShipped_VLIGON.setImageResource(item.shipped ? R.drawable.ic_shipped : R.drawable.ic_shipped_off);
 
