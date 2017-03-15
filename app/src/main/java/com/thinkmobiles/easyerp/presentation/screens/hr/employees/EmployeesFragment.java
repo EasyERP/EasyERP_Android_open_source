1007696e00978659ff00d47bd2ba68a70847be0b
roman.saldan <80323932597>
2017-03-15 15:55:53 +0200

    remove gallery test data

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/hr/employees/EmployeesFragment.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/hr/employees/EmployeesFragment.java
@@ -70,29 +70,6 @@ public class EmployeesFragment extends MasterAlphabeticalFragment implements Emp
 
     @Override
     public void openDetailsScreen(String employeeID) {
-        ArrayList<ImageItem> images = new ArrayList<>();
-        ImageItem item1 = new ImageItem();
-        ImageItem item2 = new ImageItem();
-        ImageItem item3 = new ImageItem();
-        ImageItem item4 = new ImageItem();
-        ImageItem item5 = new ImageItem();
-
-        item1.imageSrc = "/customImages/58be77c49531b78473550902/58be77c49531b78473550903.jpeg";
-        item2.imageSrc = "/customImages/58be77c49531b78473550902/58be77d89220a4c47382640a.jpeg";
-        item3.imageSrc = "/customImages/58be77c49531b78473550902/58be77e58d58517473184fc3.jpeg";
-        item4.imageSrc = "/customImages/58be77c49531b78473550902/58be77ed9531b78473550908.jpeg";
-        item5.imageSrc = "/customImages/58be77c49531b78473550902/58be77f68629ece673bba398.jpeg";
-
-        images.add(item1);
-        images.add(item2);
-        images.add(item3);
-        images.add(item4);
-        images.add(item5);
-
-        GalleryActivity_.intent(getActivity())
-                .imageItems(images)
-                .position(3)
-                .title("ZBS book")
-                .start();
+        //TODO open Employee details
     }
 }
