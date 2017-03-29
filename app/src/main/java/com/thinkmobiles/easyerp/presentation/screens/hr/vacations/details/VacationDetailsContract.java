1fdd4a06e2e74f9d825a0a93317e046bac74d8bc
roman.saldan <80323932597>
2017-03-29 16:20:26 +0300

    progress save

---
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/hr/vacations/details/VacationDetailsContract.java
@@ -0,0 +1,17 @@
+package com.thinkmobiles.easyerp.presentation.screens.hr.vacations.details;
+
+/**
+ * Created by Lynx on 3/29/2017.
+ */
+
+public interface VacationDetailsContract {
+    interface VacationDetailsView {
+
+    }
+    interface VacationDetailsPresenter {
+
+    }
+    interface VacationDetailsModel {
+
+    }
+}
---
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/screens/hr/vacations/details/VacationDetailsFragment.java
@@ -0,0 +1,8 @@
+package com.thinkmobiles.easyerp.presentation.screens.hr.vacations.details;
+
+/**
+ * Created by Lynx on 3/29/2017.
+ */
+
+public class VacationDetailsFragment {
+}
