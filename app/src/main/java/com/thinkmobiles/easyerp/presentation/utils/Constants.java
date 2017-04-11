c09c3032e8a77727fec25e1f7cff60ed7b163b8d
michael.soyma <michael.soyma@thinkmobiles.com>
2017-04-11 14:46:51 +0300

    fix key in reports request

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/utils/Constants.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/utils/Constants.java
@@ -154,7 +154,7 @@ public abstract class Constants {
     public static final String GET_REPORTS_FAVORITE         = "reports/favorite/{reportId}";
     public static final String GET_REPORTS_UNFAVORITE       = "reports/unfavorite/{reportId}";
 
-    public static final String KEY_QUERY_REPORT_CATEGORY    = "reportCategory";
+    public static final String KEY_QUERY_REPORT_CATEGORY    = "reportCategory[]";
 
     //------------------------------------------------------ END REPORTS
 
