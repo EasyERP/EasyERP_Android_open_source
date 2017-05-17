09c47b856dfbfa2daa4f1aab2cae3239792affc6
Michael Soyma <michael.soyma@thinkmobiles.com>
2017-05-17 14:07:47 +0300

    add tutorial screens

--- app/src/main/java/com/thinkmobiles/easyerp/presentation/adapters/TutorialPagerAdapter.java
+++ app/src/main/java/com/thinkmobiles/easyerp/presentation/adapters/TutorialPagerAdapter.java
@@ -1,6 +1,8 @@
 package com.thinkmobiles.easyerp.presentation.adapters;
 
 import android.app.Activity;
+import android.support.annotation.DrawableRes;
+import android.support.annotation.StringRes;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.PagerAdapter;
 import android.view.LayoutInflater;
@@ -24,6 +26,10 @@ public class TutorialPagerAdapter extends PagerAdapter {
 
     private LayoutInflater inflater;
 
+    private static final int TUTORIAL_SCREEN_COUNT = 10;
+    private @StringRes int[] titles = new int[TUTORIAL_SCREEN_COUNT];
+    private @DrawableRes int[] screens = new int[TUTORIAL_SCREEN_COUNT];
+
     @RootContext
     protected Activity activity;
 
@@ -32,9 +38,34 @@ public class TutorialPagerAdapter extends PagerAdapter {
         inflater = LayoutInflater.from(activity);
     }
 
+    @AfterInject
+    protected void initData() {
+        titles[0] = R.string.tutorial_1;
+        titles[1] = R.string.tutorial_2;
+        titles[2] = R.string.tutorial_3;
+        titles[3] = R.string.tutorial_4;
+        titles[4] = R.string.tutorial_5;
+        titles[5] = R.string.tutorial_6;
+        titles[6] = R.string.tutorial_7;
+        titles[7] = R.string.tutorial_8;
+        titles[8] = R.string.tutorial_9;
+        titles[9] = R.string.tutorial_10;
+
+        screens[0] = R.drawable.tutorial1;
+        screens[1] = R.drawable.tutorial2;
+        screens[2] = R.drawable.tutorial3;
+        screens[3] = R.drawable.tutorial4;
+        screens[4] = R.drawable.tutorial5;
+        screens[5] = R.drawable.tutorial6;
+        screens[6] = R.drawable.tutorial7;
+        screens[7] = R.drawable.tutorial8;
+        screens[8] = R.drawable.tutorial9;
+        screens[9] = R.drawable.tutorial10;
+    }
+
     @Override
     public int getCount() {
-        return 5;
+        return TUTORIAL_SCREEN_COUNT;
     }
 
     @Override
@@ -49,30 +80,13 @@ public class TutorialPagerAdapter extends PagerAdapter {
 
     private View bindData(int position, ViewGroup container) {
         View view = inflater.inflate(R.layout.view_tutorial_item, container, false);
-        TextView tvTitle_VTI = (TextView) view.findViewById(R.id.tvTitle_VTI);
-        ImageView ivTutorialImage_VTI = (ImageView) view.findViewById(R.id.ivTutorialImage_VTI);
-        switch (position) {
-            case 0:
-                tvTitle_VTI.setText(R.string.tutorial_1);
-                ivTutorialImage_VTI.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.tutorial1));
-                break;
-            case 1:
-                tvTitle_VTI.setText(R.string.tutorial_2);
-                ivTutorialImage_VTI.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.tutorial2));
-                break;
-            case 2:
-                tvTitle_VTI.setText(R.string.tutorial_3);
-                ivTutorialImage_VTI.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.tutorial3));
-                break;
-            case 3:
-                tvTitle_VTI.setText(R.string.tutorial_4);
-                ivTutorialImage_VTI.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.tutorial4));
-                break;
-            case 4:
-                tvTitle_VTI.setText(R.string.tutorial_5);
-                ivTutorialImage_VTI.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.tutorial5));
-                break;
-        }
+
+        TextView title = (TextView) view.findViewById(R.id.tvTitle_VTI);
+        ImageView screen = (ImageView) view.findViewById(R.id.ivTutorialImage_VTI);
+
+        title.setText(titles[position]);
+        screen.setImageDrawable(ContextCompat.getDrawable(activity, screens[position]));
+
         container.addView(view);
         return view;
     }
